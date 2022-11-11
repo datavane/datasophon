b2ac27b96eab49dd9a496d29d0fcb0cd6d85d87b
rwj <renwujie@utopin.cn>
2022-11-11 22:23:11 +0800

    [issues_5][Service]optimize and add test class for class HostUtils(parse file /etc/hosts)

--- datasophon-common/src/main/java/com/datasophon/common/utils/HostUtils.java
+++ datasophon-common/src/main/java/com/datasophon/common/utils/HostUtils.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -17,57 +18,58 @@ import java.util.stream.Collectors;
  *
  * @author gaodayu
  */
-public abstract class HostUtils {
+public final class HostUtils {
     private static final String HOSTS_PATH = "/etc/hosts";
-    private static final String ENDL = String.format("%n");
+
+    private static final String ENDL = "\r?\n";
+
+    private static final String ONE_OR_MORE_SPACE = "\\s+";
+    private static final String COMMENT_OR_BLANK_LINE = "(^#.*)|(\\s*)";
+    private static final String COMMENT = "#.*";
+    private static final String TABS = "\t";
 
     public static void read() {
-        List<String> list = Arrays.stream(new FileReader(HOSTS_PATH).readString().split(ENDL))
-                .filter(it -> !it.trim().matches("(^#.*)|(\\s*)"))
-                .map(it -> it.replaceAll("#.*", "").trim().replaceAll("\\s+", "\t"))
-                .collect(Collectors.toList());
+        List<String> ipHostNameList = parse2List();
+
         HashMap<String, String> ipHost = new HashMap<>();
         HashMap<String, String> hostIp = new HashMap<>();
-        for (String str : list) {
-            String[] split = str.split("\\s+");
-            if (split.length == 3) {
-                ipHost.put(split[0], split[2]);
-                hostIp.put(split[2],split[0]);
+        for (String str : ipHostNameList) {
+            String[] split = str.split(ONE_OR_MORE_SPACE);
+            int splitLength = split.length;
+            if (splitLength < 2) {
+                continue;
             }
-            if (split.length == 2) {
-                ipHost.put(split[0], split[1]);
-                hostIp.put(split[1],split[0]);
-            }
+            ipHost.put(split[0], split[splitLength - 1]);
+            hostIp.put(split[splitLength - 1], split[0]);
         }
-        CacheUtils.put(Constants.IP_HOST,ipHost);
-        CacheUtils.put(Constants.HOST_IP,hostIp);
+
+        CacheUtils.put(Constants.IP_HOST, ipHost);
+        CacheUtils.put(Constants.HOST_IP, hostIp);
     }
 
     public static String findIp(String hostname) {
-        List<String> list = Arrays.stream(new FileReader(HOSTS_PATH).readString().split(ENDL))
-                .filter(it -> !it.trim().matches("(^#.*)|(\\s*)"))
-                .map(it -> it.replaceAll("#.*", "").trim().replaceAll("\\s+", "\t"))
-                .collect(Collectors.toList());
+        List<String> ipHostNameList = parse2List();
+
         String ip = "";
-        for (String str : list) {
-            String[] split = str.split("\\s+");
-            if (split.length == 3) {
-                if(split[2].equals(hostname)){
-                    ip = split[0];
-                }
+        for (String str : ipHostNameList) {
+            String[] split = str.split(ONE_OR_MORE_SPACE);
+            if (split.length < 2) {
+                continue;
             }
-            if (split.length == 2) {
-               if(split[1] == hostname){
-                   ip = split[0];
-               }
+            if (Objects.equals(split[split.length - 1], hostname)) {
+                ip = split[0];
+                break;
             }
         }
         return ip;
     }
 
-    public static void main(String[] args) {
-        HostUtils.read();
-
+    private static List<String> parse2List() {
+        List<String> list = Arrays.stream(new FileReader(HOSTS_PATH).readString().split(ENDL))
+                .filter(it -> !it.trim().matches(COMMENT_OR_BLANK_LINE))
+                .map(it -> it.replaceAll(COMMENT, "").trim().replaceAll(ONE_OR_MORE_SPACE, TABS))
+                .collect(Collectors.toList());
+        return list;
     }
 
 }
