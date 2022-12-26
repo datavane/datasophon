5b3c803ad7b811de6d83dd7eeff72d8fe7fbafee
dygao2 <dygao2@iflytek.com>
2022-12-26 16:35:12 +0800

    add kerberos alert

--- datasophon-api/src/main/java/com/datasophon/api/master/alert/ServiceRoleCheckActor.java
+++ datasophon-api/src/main/java/com/datasophon/api/master/alert/ServiceRoleCheckActor.java
@@ -1,24 +1,37 @@
 package com.datasophon.api.master.alert;
 
+import akka.actor.ActorSelection;
 import akka.actor.UntypedActor;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
 import cn.hutool.http.HttpUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.datasophon.api.load.ServiceRoleMap;
+import com.datasophon.api.master.ActorUtils;
 import com.datasophon.api.service.ClusterAlertHistoryService;
+import com.datasophon.api.service.ClusterInfoService;
 import com.datasophon.api.service.ClusterServiceInstanceService;
 import com.datasophon.api.utils.SpringTool;
 import com.datasophon.api.service.ClusterServiceRoleInstanceService;
 import com.datasophon.common.Constants;
+import com.datasophon.common.command.ExecuteCmdCommand;
 import com.datasophon.common.command.ServiceRoleCheckCommand;
+import com.datasophon.common.model.ServiceRoleInfo;
+import com.datasophon.common.utils.ExecResult;
 import com.datasophon.dao.entity.ClusterAlertHistory;
+import com.datasophon.dao.entity.ClusterInfoEntity;
 import com.datasophon.dao.entity.ClusterServiceInstanceEntity;
 import com.datasophon.dao.entity.ClusterServiceRoleInstanceEntity;
 import com.datasophon.dao.enums.AlertLevel;
 import com.datasophon.dao.enums.ServiceRoleState;
 import com.datasophon.dao.enums.ServiceState;
+import org.apache.commons.lang.StringUtils;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class ServiceRoleCheckActor extends UntypedActor {
 
@@ -37,33 +50,64 @@ public class ServiceRoleCheckActor extends UntypedActor {
     public void onReceive(Object msg) throws Throwable {
         if (msg instanceof ServiceRoleCheckCommand) {//定时检测prometheus和alertmanager
             ClusterServiceRoleInstanceService roleInstanceService = SpringTool.getApplicationContext().getBean(ClusterServiceRoleInstanceService.class);
+            ClusterInfoService clusterInfoService = SpringTool.getApplicationContext().getBean(ClusterInfoService.class);
 
             List<ClusterServiceRoleInstanceEntity> list = roleInstanceService.list(new QueryWrapper<ClusterServiceRoleInstanceEntity>()
                     .in(Constants.SERVICE_ROLE_NAME, "Prometheus", "AlertManager"));
+            String frameCode = "";
             if (Objects.nonNull(list) && list.size() > 0) {
                 for (ClusterServiceRoleInstanceEntity roleInstanceEntity : list) {
-                    if("Prometheus".equals(roleInstanceEntity.getServiceRoleName())){
-                        String url = "http://"+roleInstanceEntity.getHostname()+":9090";
+                    if ("Prometheus".equals(roleInstanceEntity.getServiceRoleName())) {
+                        String url = "http://" + roleInstanceEntity.getHostname() + ":9090";
                         try {
                             HttpUtil.get(url);
                             //恢复告警
-                            recoverAlert(roleInstanceEntity,roleInstanceService);
-                        }catch (Exception e){
-                            saveAlert(roleInstanceEntity,roleInstanceService);
+                            recoverAlert(roleInstanceEntity, roleInstanceService);
+                        } catch (Exception e) {
+                            saveAlert(roleInstanceEntity, roleInstanceService);
                         }
                     }
-                    if("AlertManager".equals(roleInstanceEntity.getServiceRoleName())){
-                        String url = "http://"+roleInstanceEntity.getHostname()+":9093";
+                    if ("AlertManager".equals(roleInstanceEntity.getServiceRoleName())) {
+                        String url = "http://" + roleInstanceEntity.getHostname() + ":9093";
                         try {
                             HttpUtil.get(url);
-                            recoverAlert(roleInstanceEntity,roleInstanceService);
-                        }catch (Exception e){
+                            recoverAlert(roleInstanceEntity, roleInstanceService);
+                        } catch (Exception e) {
                             //产生告警
-                            saveAlert(roleInstanceEntity,roleInstanceService);
+                            saveAlert(roleInstanceEntity, roleInstanceService);
 
                         }
                     }
+                    if ("Krb5Kdc".equals(roleInstanceEntity.getServiceRoleName()) ||
+                            "KAdmin".equals(roleInstanceEntity.getServiceRoleName())) {
+                        Integer clusterId = roleInstanceEntity.getClusterId();
+                        if (StringUtils.isBlank(frameCode)) {
+                            ClusterInfoEntity cluster = clusterInfoService.getById(clusterId);
+                            frameCode = cluster.getClusterFrame();
+                        }
+                        String key = frameCode + Constants.UNDERLINE + roleInstanceEntity.getServiceName() + Constants.UNDERLINE + roleInstanceEntity.getServiceRoleName();
+                        ServiceRoleInfo serviceRoleInfo = ServiceRoleMap.get(key);
 
+                        ActorSelection execCmdActor = ActorUtils.actorSystem.actorSelection("akka.tcp://datasophon@" + roleInstanceEntity.getHostname() + ":2552/user/worker/executeCmdActor");
+                        ExecuteCmdCommand cmdCommand = new ExecuteCmdCommand();
+                        ArrayList<String> commandList = new ArrayList<>();
+                        commandList.add(serviceRoleInfo.getStatusRunner().getProgram());
+                        commandList.addAll(serviceRoleInfo.getStatusRunner().getArgs());
+                        cmdCommand.setCommands(commandList);
+                        Timeout timeout = new Timeout(Duration.create(30, TimeUnit.SECONDS));
+                        Future<Object> execFuture = Patterns.ask(execCmdActor, cmdCommand, timeout);
+                        try {
+                            ExecResult execResult = (ExecResult) Await.result(execFuture, timeout.duration());
+                            if (execResult.getExecResult()) {
+                                recoverAlert(roleInstanceEntity, roleInstanceService);
+                            } else {
+                                saveAlert(roleInstanceEntity, roleInstanceService);
+                            }
+                        } catch (Exception e) {
+                            //save alert
+                            saveAlert(roleInstanceEntity, roleInstanceService);
+                        }
+                    }
                 }
             }
         } else {
@@ -74,47 +118,42 @@ public class ServiceRoleCheckActor extends UntypedActor {
     private void recoverAlert(ClusterServiceRoleInstanceEntity roleInstanceEntity, ClusterServiceRoleInstanceService roleInstanceService) {
         ClusterAlertHistoryService alertHistoryService = SpringTool.getApplicationContext().getBean(ClusterAlertHistoryService.class);
         ClusterAlertHistory clusterAlertHistory = alertHistoryService.getOne(new QueryWrapper<ClusterAlertHistory>()
-                .eq(Constants.ALERT_TARGET_NAME, roleInstanceEntity.getServiceRoleName()+"进程存活")
+                .eq(Constants.ALERT_TARGET_NAME, roleInstanceEntity.getServiceRoleName() + " Survive")
                 .eq(Constants.CLUSTER_ID, roleInstanceEntity.getClusterId())
-                .eq(Constants.HOSTNAME,roleInstanceEntity.getHostname())
-                .eq(Constants.IS_ENABLED,1));
-        if(Objects.nonNull(clusterAlertHistory)){
+                .eq(Constants.HOSTNAME, roleInstanceEntity.getHostname())
+                .eq(Constants.IS_ENABLED, 1));
+        if (Objects.nonNull(clusterAlertHistory)) {
             clusterAlertHistory.setIsEnabled(2);
             alertHistoryService.updateById(clusterAlertHistory);
         }
         //更改服务角色实例状态
-        if(roleInstanceEntity.getServiceRoleState() != ServiceRoleState.RUNNING){
+        if (roleInstanceEntity.getServiceRoleState() != ServiceRoleState.RUNNING) {
             roleInstanceEntity.setServiceRoleState(ServiceRoleState.RUNNING);
             roleInstanceService.updateById(roleInstanceEntity);
         }
     }
 
-    private void saveAlert(ClusterServiceRoleInstanceEntity roleInstanceEntity,ClusterServiceRoleInstanceService roleInstanceService) {
+    private void saveAlert(ClusterServiceRoleInstanceEntity roleInstanceEntity, ClusterServiceRoleInstanceService roleInstanceService) {
         ClusterAlertHistoryService alertHistoryService = SpringTool.getApplicationContext().getBean(ClusterAlertHistoryService.class);
         ClusterServiceInstanceService serviceInstanceService = SpringTool.getApplicationContext().getBean(ClusterServiceInstanceService.class);
         ClusterAlertHistory clusterAlertHistory = alertHistoryService.getOne(new QueryWrapper<ClusterAlertHistory>()
-                .eq(Constants.ALERT_TARGET_NAME, roleInstanceEntity.getServiceRoleName()+"进程存活")
+                .eq(Constants.ALERT_TARGET_NAME, roleInstanceEntity.getServiceRoleName() + " Survive")
                 .eq(Constants.CLUSTER_ID, roleInstanceEntity.getClusterId())
-                .eq(Constants.HOSTNAME,roleInstanceEntity.getHostname())
-                .eq(Constants.IS_ENABLED,1));
+                .eq(Constants.HOSTNAME, roleInstanceEntity.getHostname())
+                .eq(Constants.IS_ENABLED, 1));
 
         ClusterServiceInstanceEntity serviceInstanceEntity = serviceInstanceService.getById(roleInstanceEntity.getServiceId());
-        if(Objects.isNull(clusterAlertHistory)){
+        if (Objects.isNull(clusterAlertHistory)) {
             clusterAlertHistory = new ClusterAlertHistory();
             clusterAlertHistory.setClusterId(roleInstanceEntity.getClusterId());
-            if("Prometheus".equals(roleInstanceEntity.getServiceRoleName())){
-                clusterAlertHistory.setAlertGroupName("prometheus");
-                clusterAlertHistory.setAlertTargetName("Prometheus进程存活");
-            }
-            if("AlertManager".equals(roleInstanceEntity.getServiceRoleName())){
-                clusterAlertHistory.setAlertGroupName("alertmanager");
-                clusterAlertHistory.setAlertTargetName("AlertManager进程存活");
-            }
+
+            clusterAlertHistory.setAlertGroupName(roleInstanceEntity.getServiceName().toLowerCase());
+            clusterAlertHistory.setAlertTargetName(roleInstanceEntity.getServiceRoleName()+" Survive");
             clusterAlertHistory.setCreateTime(new Date());
             clusterAlertHistory.setUpdateTime(new Date());
             clusterAlertHistory.setAlertLevel(AlertLevel.EXCEPTION);
             clusterAlertHistory.setAlertInfo("");
-            clusterAlertHistory.setAlertAdvice("重新启动");
+            clusterAlertHistory.setAlertAdvice("restart");
             clusterAlertHistory.setHostname(roleInstanceEntity.getHostname());
             clusterAlertHistory.setIsEnabled(1);
 
