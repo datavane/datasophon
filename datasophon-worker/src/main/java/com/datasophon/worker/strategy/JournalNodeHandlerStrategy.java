275bf31bd832dab315e2058e645ea7f955056920
dygao2 <dygao2@iflytek.com>
2023-01-13 17:41:25 +0800

    auth generate keystore and truststore

--- datasophon-worker/src/main/java/com/datasophon/worker/strategy/JournalNodeHandlerStrategy.java
+++ datasophon-worker/src/main/java/com/datasophon/worker/strategy/JournalNodeHandlerStrategy.java
@@ -24,18 +24,33 @@ public class JournalNodeHandlerStrategy implements ServiceRoleStrategy {
     public ExecResult handler(ServiceRoleOperateCommand command) throws SQLException, ClassNotFoundException {
         ExecResult startResult = new ExecResult();
         ServiceHandler serviceHandler = new ServiceHandler();
-        if(command.getEnableKerberos()){
+        if (command.getEnableKerberos()) {
             logger.info("start to get journalnode keytab file");
             String hostname = CacheUtils.getString(Constants.HOSTNAME);
             KerberosUtils.createKeytabDir();
-            if(!FileUtil.exist("/etc/security/keytab/jn.service.keytab")){
+            String hadoopConfDir = Constants.INSTALL_PATH + Constants.SLASH + command.getDecompressPackageName() + "/etc/hadoop/";
+            if (!FileUtil.exist(hadoopConfDir + "ssl-server.xml")) {
+                ShellUtils.exceShell("cp ssl-server.xml.example ssl-server.xml");
+            }
+            if (!FileUtil.exist(hadoopConfDir + "ssl-client.xml")) {
+                ShellUtils.exceShell("cp ssl-client.xml.example ssl-client.xml");
+            }
+            if (!FileUtil.exist("/etc/security/keytab/jn.service.keytab")) {
                 KerberosUtils.downloadKeytabFromMaster("jn/" + hostname, "jn.service.keytab");
             }
-            if(!FileUtil.exist("//etc/security/keytab/keystore")){
-                ShellUtils.exceShell("keytool -genkey -alias datasophon -keypass admin123 -keyalg RSA -keysize 1024 -validity 365 -keystore  /etc/security/keytab/keystore -storepass admin123 -dname \"CN="+hostname+", OU=, O=, L=, ST=,C=\";");
+            if (!FileUtil.exist("/etc/security/keytab/keystore")) {
+                ArrayList<String> commands = new ArrayList<>();
+                commands.add("sh");
+                commands.add("keystore.sh");
+                commands.add(hostname);
+                ExecResult execResult = ShellUtils.execWithStatus(Constants.WORKER_SCRIPT_PATH, commands, 30L);
+                if (!execResult.getExecResult()) {
+                    logger.info("generate keystore file failed");
+                    return execResult;
+                }
             }
             startResult = serviceHandler.start(command.getStartRunner(), command.getStatusRunner(), command.getDecompressPackageName(), command.getRunAs());
-        }else {
+        } else {
             startResult = serviceHandler.start(command.getStartRunner(), command.getStatusRunner(), command.getDecompressPackageName(), command.getRunAs());
         }
         return startResult;
