30d4ae37508022f284c27642e43e4a71a5b1dd65
dygao2 <dygao2@iflytek.com>
2023-02-03 11:37:11 +0800

    [feature] add hbase kerberos config

--- datasophon-worker/src/main/java/com/datasophon/worker/strategy/HbaseHandlerStrategy.java
+++ datasophon-worker/src/main/java/com/datasophon/worker/strategy/HbaseHandlerStrategy.java
@@ -25,22 +25,22 @@ public class HbaseHandlerStrategy implements ServiceRoleStrategy {
             ArrayList<String> commands = new ArrayList<>();
             commands.add("sh");
             commands.add("./enable-hbase-plugin.sh");
-            if (!FileUtil.exist(Constants.INSTALL_PATH + "/hbase-2.0.2/ranger-hbase-plugin/success.id")) {
-                ExecResult execResult = ShellUtils.execWithStatus(Constants.INSTALL_PATH + "/hbase-2.0.2/ranger-hbase-plugin", commands, 30L);
+            if (!FileUtil.exist(Constants.INSTALL_PATH + Constants.SLASH + command.getDecompressPackageName() + "/ranger-hbase-plugin/success.id")) {
+                ExecResult execResult = ShellUtils.execWithStatus(Constants.INSTALL_PATH + Constants.SLASH + command.getDecompressPackageName() + "/ranger-hbase-plugin", commands, 30L);
                 if (execResult.getExecResult()) {
                     logger.info("enable ranger hbase plugin success");
-                    FileUtil.writeUtf8String("success", Constants.INSTALL_PATH + "/hbase-2.0.2/ranger-hbase-plugin/success.id");
+                    FileUtil.writeUtf8String("success", Constants.INSTALL_PATH + Constants.SLASH + command.getDecompressPackageName() + "/ranger-hbase-plugin/success.id");
                 } else {
                     logger.info("enable ranger hbase plugin failed");
                     return execResult;
                 }
             }
         }
-        if(command.getEnableKerberos()){
+        if (command.getEnableKerberos()) {
             logger.info("start to get hbase keytab file");
             String hostname = CacheUtils.getString(Constants.HOSTNAME);
             KerberosUtils.createKeytabDir();
-            if(!FileUtil.exist("/etc/security/keytab/hbase.keytab")){
+            if (!FileUtil.exist("/etc/security/keytab/hbase.keytab")) {
                 KerberosUtils.downloadKeytabFromMaster("hbase/" + hostname, "hbase.keytab");
             }
         }
