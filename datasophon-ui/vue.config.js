44f1b8f6f52b59f958cef865476c961cdafc7335
huangli <huangli@iflytek.com>
2023-02-23 16:21:08 +0800

    feat：publicPath设置（/${path},后端配置）

--- datasophon-ui/vue.config.js
+++ datasophon-ui/vue.config.js
@@ -1,8 +1,8 @@
 let path = require('path')
 const webpack = require('webpack')
 const ThemeColorReplacer = require('webpack-theme-color-replacer')
-const {getThemeColors, modifyVars} = require('./src/utils/themeUtil')
-const {resolveCss} = require('./src/utils/theme-color-replacer-extend')
+const { getThemeColors, modifyVars } = require('./src/utils/themeUtil')
+const { resolveCss } = require('./src/utils/theme-color-replacer-extend')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 // const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
@@ -24,10 +24,9 @@ const assetsCDN = {
     nprogress: 'NProgress',
     clipboard: 'ClipboardJS',
     // '@antv/data-set': 'DataSet',
-    'js-cookie': 'Cookies'
+    'js-cookie': 'Cookies',
   },
-  css: [
-  ],
+  css: [],
   js: [
     // '//cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js',
     // '//cdn.jsdelivr.net/npm/vue-router@3.3.4/dist/vue-router.min.js',
@@ -43,41 +42,42 @@ const assetsCDN = {
     '//cdn.bootcdn.net/ajax/libs/axios/0.19.0/axios.min.js',
     '//cdn.bootcdn.net/ajax/libs/nprogress/0.2.0/nprogress.js',
     '//cdn.bootcdn.net/ajax/libs/clipboard.js/2.0.11/clipboard.min.js',
-    '//cdn.bootcdn.net/ajax/libs/js-cookie/3.0.1/js.cookie.min.js'
-  ]
+    '//cdn.bootcdn.net/ajax/libs/js-cookie/3.0.1/js.cookie.min.js',
+  ],
 }
 
 module.exports = {
-  // 172.31.96.16:5001 
+  // 172.31.96.16:5001
   devServer: {
     port: 8080,
     proxy: {
-      '/ddh': { //此处要与 /services/api.js 中的 API_PROXY_PREFIX 值保持一致
+      '/ddh': {
+        //此处要与 /services/api.js 中的 API_PROXY_PREFIX 值保持一致
         target: process.env.VUE_APP_API_BASE_URL,
         changeOrigin: true,
         pathRewrite: {
-          '^/api': '/ddh'
-        }
-      }
-    }
+          '^/api': '/ddh',
+        },
+      },
+    },
   },
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
-      patterns: [path.resolve(__dirname, "./src/assets/less/theme.less")],
-    }
+      patterns: [path.resolve(__dirname, './src/assets/less/theme.less')],
+    },
   },
-  configureWebpack: config => {
-    config.entry.app = ["babel-polyfill", "whatwg-fetch", "./src/main.js"];
+  configureWebpack: (config) => {
+    config.entry.app = ['babel-polyfill', 'whatwg-fetch', './src/main.js']
     config.performance = {
-      hints: false
+      hints: false,
     }
     config.plugins.push(
       new ThemeColorReplacer({
         fileName: 'css/theme-colors-[contenthash:8].css',
         matchColors: getThemeColors(),
         injectCss: true,
-        resolveCss
+        resolveCss,
       })
     )
     // Ignore all locale files of moment.js
@@ -85,12 +85,14 @@ module.exports = {
     // 生产环境下将资源压缩成gzip格式
     if (isProd) {
       // add `CompressionWebpack` plugin to webpack plugins
-      config.plugins.push(new CompressionWebpackPlugin({
-        algorithm: 'gzip',
-        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-        threshold: 10240,
-        minRatio: 0.8
-      }))
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+          threshold: 10240,
+          minRatio: 0.8,
+        })
+      )
       config.plugins.push(
         //打包环境去掉console.log
         new UglifyJsPlugin({
@@ -102,7 +104,7 @@ module.exports = {
               pure_funcs: ['console.log'], //移除console.log
             },
           },
-        }),
+        })
       )
     }
     // if prod, add externals
@@ -111,20 +113,17 @@ module.exports = {
     }
     config.plugins.push(new MonacoWebpackPlugin())
     // config.module = {
-		// 	rules: [{
-		// 		test: /\.js/,
-		// 		enforce: 'pre',
-		// 		include: /node_modules[\\\/]monaco-editor[\\\/]esm/,
-		// 		use: MonacoWebpackPlugin.loader
-		// 	}]
+    // 	rules: [{
+    // 		test: /\.js/,
+    // 		enforce: 'pre',
+    // 		include: /node_modules[\\\/]monaco-editor[\\\/]esm/,
+    // 		use: MonacoWebpackPlugin.loader
+    // 	}]
     // }
   },
   chainWebpack(config) {
-     // 第一步：让其他svg loader不要对路径下进行操作
-    config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
+    // 第一步：让其他svg loader不要对路径下进行操作
+    config.module.rule('svg').exclude.add(resolve('src/icons')).end()
     config.module
       .rule('icons')
       .test(/\.svg$/)
@@ -135,14 +134,13 @@ module.exports = {
       .options({
         symbolId: 'icon-[name]',
       })
-     .end()
+      .end()
     // 生产环境下关闭css压缩的 colormin 项，因为此项优化与主题色替换功能冲突
     if (isProd) {
-      config.plugin('optimize-css')
-        .tap(args => {
-          args[0].cssnanoOptions.preset[1].colormin = false
-          return args
-        })
+      config.plugin('optimize-css').tap((args) => {
+        args[0].cssnanoOptions.preset[1].colormin = false
+        return args
+      })
     }
     // 生产环境下使用CDN
     if (isProd) {
@@ -158,13 +156,16 @@ module.exports = {
       less: {
         lessOptions: {
           modifyVars: modifyVars(),
-          javascriptEnabled: true
-        }
-      }
-    }
+          javascriptEnabled: true,
+        },
+      },
+    },
   },
-  publicPath: process.env.VUE_APP_PUBLIC_PATH,
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/${path}'
+      : process.env.VUE_APP_PUBLIC_PATH,
   outputDir: 'dist',
   assetsDir: 'static',
-  productionSourceMap: false
+  productionSourceMap: false,
 }
