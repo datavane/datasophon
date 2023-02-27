2d3eb2f78d4eecebcb03fc5c12c306149d4373ad
renwujie <rwj005@outlook.com>
2023-02-27 11:40:56 +0800

    perf: Remove constant Constants.SERVICE_ID and Constants.SERVICE_ROLE_TYPE. (#174)
    * perf: Remove constant Constants.COMMAND_ID, and use SFunction instead.
    
    * perf: Remove constant Constants.SERVICE_ID and Constants.SERVICE_ROLE_TYPE, and use SFunction instead.

--- datasophon-api/src/main/java/com/datasophon/api/master/HdfsECActor.java
+++ datasophon-api/src/main/java/com/datasophon/api/master/HdfsECActor.java
@@ -1,11 +1,9 @@
 package com.datasophon.api.master;
 
 import akka.actor.UntypedActor;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.datasophon.api.utils.ProcessUtils;
 import com.datasophon.api.utils.SpringTool;
 import com.datasophon.api.service.ClusterServiceRoleInstanceService;
-import com.datasophon.common.Constants;
 import com.datasophon.common.command.HdfsEcCommand;
 import com.datasophon.dao.entity.ClusterServiceRoleInstanceEntity;
 import org.slf4j.Logger;
@@ -19,23 +17,24 @@ import java.util.stream.Collectors;
  * Used to manage hdfs capacity expansion and reduction
  */
 public class HdfsECActor extends UntypedActor {
+
     private static final Logger logger = LoggerFactory.getLogger(HdfsECActor.class);
 
     @Override
-    public void onReceive(Object msg) throws Throwable, Throwable {
-        if(msg instanceof HdfsEcCommand){
-
+    public void onReceive(Object msg) throws Throwable {
+        if (msg instanceof HdfsEcCommand) {
             HdfsEcCommand hdfsEcCommand = (HdfsEcCommand) msg;
             ClusterServiceRoleInstanceService roleInstanceService = SpringTool.getApplicationContext().getBean(ClusterServiceRoleInstanceService.class);
             //list datanode
-            List<ClusterServiceRoleInstanceEntity> datanodes = roleInstanceService.list(new QueryWrapper<ClusterServiceRoleInstanceEntity>().eq(Constants.SERVICE_ID, hdfsEcCommand.getServiceInstanceId()).eq(Constants.SERVICE_ROLE_NAME, "DataNode"));
+            List<ClusterServiceRoleInstanceEntity> datanodes = roleInstanceService.lambdaQuery()
+                    .eq(ClusterServiceRoleInstanceEntity::getServiceId, hdfsEcCommand.getServiceInstanceId())
+                    .eq(ClusterServiceRoleInstanceEntity::getServiceRoleName, "DataNode")
+                    .list();
             TreeSet<String> list = datanodes.stream().map(e -> e.getHostname()).collect(Collectors.toCollection(TreeSet::new));
-            ProcessUtils.hdfsEcMethond(hdfsEcCommand.getServiceInstanceId(),roleInstanceService,list,"whitelist","NameNode");
-
-        }else {
+            ProcessUtils.hdfsEcMethond(hdfsEcCommand.getServiceInstanceId(), roleInstanceService, list, "whitelist", "NameNode");
+        } else {
             unhandled(msg);
         }
     }
-
 
 }
