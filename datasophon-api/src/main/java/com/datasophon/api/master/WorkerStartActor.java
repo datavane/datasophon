23465890da1e30d35f5388a6da4aa6e9eaf709ec
gaodayu168 <gaodayu2022@163.com>
2023-03-31 14:05:02 +0800

    [improvement][master] sync user and group when add a new worker node

--- datasophon-api/src/main/java/com/datasophon/api/master/WorkerStartActor.java
+++ datasophon-api/src/main/java/com/datasophon/api/master/WorkerStartActor.java
@@ -19,30 +19,35 @@ package com.datasophon.api.master;
 
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
 import cn.hutool.core.util.ObjectUtil;
-import com.datasophon.api.service.ClusterHostService;
-import com.datasophon.api.service.ClusterInfoService;
-import com.datasophon.api.service.ClusterServiceCommandService;
-import com.datasophon.api.service.ClusterServiceRoleInstanceService;
+import com.datasophon.api.exceptions.ServiceException;
+import com.datasophon.api.service.*;
 import com.datasophon.api.utils.ProcessUtils;
 import com.datasophon.api.utils.SpringTool;
 import com.datasophon.common.Constants;
 import com.datasophon.common.cache.CacheUtils;
 import com.datasophon.common.command.GenerateHostPrometheusConfig;
+import com.datasophon.common.command.remote.CreateUnixGroupCommand;
 import com.datasophon.common.enums.CommandType;
 import com.datasophon.common.enums.InstallState;
 import com.datasophon.common.model.HostInfo;
 import com.datasophon.common.model.StartWorkerMessage;
-import com.datasophon.dao.entity.ClusterHostEntity;
-import com.datasophon.dao.entity.ClusterInfoEntity;
-import com.datasophon.dao.entity.ClusterServiceRoleInstanceEntity;
+import com.datasophon.common.utils.ExecResult;
+import com.datasophon.dao.entity.*;
 import com.datasophon.dao.enums.MANAGED;
 import com.datasophon.dao.enums.ServiceRoleState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class WorkerStartActor extends UntypedActor {
@@ -53,18 +58,20 @@ public class WorkerStartActor extends UntypedActor {
     public void onReceive(Object message) throws Throwable {
         if (message instanceof StartWorkerMessage) {
             StartWorkerMessage msg = (StartWorkerMessage) message;
-            logger.info("receive message when worker first start :{}", msg.getHostname());
+            String hostname = msg.getHostname();
+            Integer clusterId = msg.getClusterId();
+            logger.info("receive message when worker first start :{}", hostname);
 
             ClusterHostService clusterHostService = SpringTool.getApplicationContext().getBean(ClusterHostService.class);
             ClusterInfoService clusterInfoService = SpringTool.getApplicationContext().getBean(ClusterInfoService.class);
 
             //is managed?
-            ClusterHostEntity hostEntity = clusterHostService.getClusterHostByHostname(msg.getHostname());
-            ClusterInfoEntity cluster = clusterInfoService.getById(msg.getClusterId());
+            ClusterHostEntity hostEntity = clusterHostService.getClusterHostByHostname(hostname);
+            ClusterInfoEntity cluster = clusterInfoService.getById(clusterId);
             logger.info("host install set to 100%");
             if (CacheUtils.constainsKey(cluster.getClusterCode() + Constants.HOST_MAP)) {
                 HashMap<String, HostInfo> map = (HashMap<String, HostInfo>) CacheUtils.get(cluster.getClusterCode() + Constants.HOST_MAP);
-                HostInfo hostInfo = map.get(msg.getHostname());
+                HostInfo hostInfo = map.get(hostname);
                 if (Objects.nonNull(hostInfo)) {
                     hostInfo.setProgress(Constants.ONE_HUNDRRD);
                     hostInfo.setInstallState(InstallState.SUCCESS);
@@ -76,19 +83,36 @@ public class WorkerStartActor extends UntypedActor {
                 //save to db
                 ProcessUtils.saveHostInstallInfo(msg, cluster.getClusterCode(), clusterHostService);
                 logger.info("host install save to database");
+                //sync cluster user and group
+                syncClusterUserAndGroup(clusterId, hostname);
             } else {
                 hostEntity.setCpuArchitecture(msg.getCpuArchitecture());
                 hostEntity.setManaged(MANAGED.YES);
                 clusterHostService.updateById(hostEntity);
             }
-            //tell to worker what need to start
 
 
             //add to prometheus
-            ActorRef prometheusActor = ActorUtils.getLocalActor(PrometheusActor.class,ActorUtils.getActorRefName(PrometheusActor.class));
+            ActorRef prometheusActor = ActorUtils.getLocalActor(PrometheusActor.class, ActorUtils.getActorRefName(PrometheusActor.class));
             GenerateHostPrometheusConfig prometheusConfigCommand = new GenerateHostPrometheusConfig();
             prometheusConfigCommand.setClusterId(cluster.getId());
             prometheusActor.tell(prometheusConfigCommand, getSelf());
         }
     }
+
+    private void syncClusterUserAndGroup(Integer clusterId, String hostname) {
+        ClusterGroupService clusterGroupService = SpringTool.getApplicationContext().getBean(ClusterGroupService.class);
+        ClusterUserService clusterUserService = SpringTool.getApplicationContext().getBean(ClusterUserService.class);
+
+        List<ClusterGroup> userGroupList = clusterGroupService.listAllUserGroup(clusterId);
+        for (ClusterGroup clusterGroup : userGroupList) {
+            String groupName = clusterGroup.getGroupName();
+            clusterGroupService.createUnixGroupOnHost(hostname, groupName);
+        }
+        List<ClusterUser> userList = clusterUserService.listAllUser(clusterId);
+        for (ClusterUser clusterUser : userList) {
+            clusterUserService.createUnixUserOnHost(clusterUser, hostname);
+        }
+    }
+
 }
