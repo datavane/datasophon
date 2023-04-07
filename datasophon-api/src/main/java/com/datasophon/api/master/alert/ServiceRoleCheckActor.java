4b7ba27198d8fc6294c4a7880b6ec8d0af30b062
gaodayu168 <gaodayu2022@163.com>
2023-04-07 09:53:00 +0800

    [improvement][api] update to isEmpty

--- datasophon-api/src/main/java/com/datasophon/api/master/alert/ServiceRoleCheckActor.java
+++ datasophon-api/src/main/java/com/datasophon/api/master/alert/ServiceRoleCheckActor.java
@@ -1,4 +1,5 @@
 /*
+ *
  *  Licensed to the Apache Software Foundation (ASF) under one or more
  *  contributor license agreements.  See the NOTICE file distributed with
  *  this work for additional information regarding copyright ownership.
@@ -13,41 +14,60 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *
  */
 
 package com.datasophon.api.master.alert;
 
-import akka.actor.UntypedActor;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.datasophon.api.service.ClusterServiceRoleInstanceService;
 import com.datasophon.api.strategy.ServiceRoleStrategy;
 import com.datasophon.api.strategy.ServiceRoleStrategyContext;
 import com.datasophon.api.utils.SpringTool;
-import com.datasophon.api.service.ClusterServiceRoleInstanceService;
 import com.datasophon.common.Constants;
 import com.datasophon.common.command.ServiceRoleCheckCommand;
 import com.datasophon.dao.entity.ClusterServiceRoleInstanceEntity;
 
-
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import akka.actor.UntypedActor;
+
 public class ServiceRoleCheckActor extends UntypedActor {
-
-
 
     @Override
     public void onReceive(Object msg) throws Throwable {
         if (msg instanceof ServiceRoleCheckCommand) {
-            ClusterServiceRoleInstanceService roleInstanceService = SpringTool.getApplicationContext().getBean(ClusterServiceRoleInstanceService.class);
+            ClusterServiceRoleInstanceService roleInstanceService =
+                    SpringTool.getApplicationContext()
+                            .getBean(ClusterServiceRoleInstanceService.class);
 
-            List<ClusterServiceRoleInstanceEntity> list = roleInstanceService.list(new QueryWrapper<ClusterServiceRoleInstanceEntity>()
-                    .in(Constants.SERVICE_ROLE_NAME, "Prometheus", "AlertManager", "Krb5Kdc", "KAdmin", "SRFE", "SRBE", "DorisFE", "DorisBE", "NameNode", "ResourceManager"));
+            List<ClusterServiceRoleInstanceEntity> list =
+                    roleInstanceService.list(
+                            new QueryWrapper<ClusterServiceRoleInstanceEntity>()
+                                    .in(
+                                            Constants.SERVICE_ROLE_NAME,
+                                            "Prometheus",
+                                            "AlertManager",
+                                            "Krb5Kdc",
+                                            "KAdmin",
+                                            "SRFE",
+                                            "SRBE",
+                                            "DorisFE",
+                                            "DorisBE",
+                                            "NameNode",
+                                            "ResourceManager"));
 
             Map<String, ClusterServiceRoleInstanceEntity> map = translateListToMap(list);
 
             if (!list.isEmpty()) {
                 for (ClusterServiceRoleInstanceEntity roleInstanceEntity : list) {
-                    ServiceRoleStrategy serviceRoleHandler = ServiceRoleStrategyContext.getServiceRoleHandler(roleInstanceEntity.getServiceRoleName());
+                    ServiceRoleStrategy serviceRoleHandler =
+                            ServiceRoleStrategyContext.getServiceRoleHandler(
+                                    roleInstanceEntity.getServiceRoleName());
                     if (Objects.nonNull(serviceRoleHandler)) {
                         serviceRoleHandler.handlerServiceRoleCheck(roleInstanceEntity, map);
                     }
@@ -58,9 +78,13 @@ public class ServiceRoleCheckActor extends UntypedActor {
         }
     }
 
-    private Map<String, ClusterServiceRoleInstanceEntity> translateListToMap(List<ClusterServiceRoleInstanceEntity> list) {
-        return list.stream().collect(Collectors.toMap(e -> e.getHostname() + e.getServiceRoleName(), e -> e, (v1, v2) -> v1));
+    private Map<String, ClusterServiceRoleInstanceEntity> translateListToMap(
+            List<ClusterServiceRoleInstanceEntity> list) {
+        return list.stream()
+                .collect(
+                        Collectors.toMap(
+                                e -> e.getHostname() + e.getServiceRoleName(),
+                                e -> e,
+                                (v1, v2) -> v1));
     }
-
-
 }
