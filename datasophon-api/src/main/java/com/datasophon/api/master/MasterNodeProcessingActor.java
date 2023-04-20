8d504b4d11415068c124e88dc6f6e141fd281477
zjdw <zj890404@163.com>
2023-04-20 21:03:35 +0800

    fix(bug): remove if judge of operate type.

--- datasophon-api/src/main/java/com/datasophon/api/master/MasterNodeProcessingActor.java
+++ datasophon-api/src/main/java/com/datasophon/api/master/MasterNodeProcessingActor.java
@@ -20,33 +20,31 @@ public class MasterNodeProcessingActor extends UntypedActor {
     public void onReceive(Object message) throws Throwable {
         if (message instanceof StarrocksSqlExecCommand) {
             StarrocksSqlExecCommand command = (StarrocksSqlExecCommand) message;
-            if (Constants.ADD_BE.equals(command.getOpsType())) {
-                ExecResult execResult = Constants.ADD_BE.equals(command.getOpsType())
-                        ? StarRocksUtils.addBackendSqlClient(command.getFeMaster(), command.getHostName())
-                        : StarRocksUtils.addFollowerBySqlClient(command.getFeMaster(), command.getHostName());
-                String tip = Constants.ADD_BE.equals(command.getOpsType()) ? "backend" : "follower";
-                if (execResult.getExecResult()) {
-                    logger.info(command.getHostName() + " " + tip + " be added success");
-                } else {
-                    logger.info(command.getHostName() + " " + tip + " be added failed");
-                }
-                int tryTimes = 0;
-                while (!execResult.getExecResult() && tryTimes < 3) {
-                    try {
-                        TimeUnit.SECONDS.sleep(10L);
-                        execResult = Constants.ADD_BE.equals(command.getOpsType())
-                                ? StarRocksUtils.addBackendSqlClient(command.getFeMaster(), command.getHostName())
-                                : StarRocksUtils.addFollowerBySqlClient(command.getFeMaster(), command.getHostName());
-                        if (execResult.getExecResult()) {
-                            logger.info(command.getHostName() + " " + tip + " be added success");
-                            break;
-                        } else {
-                            logger.info(command.getHostName() + " " + tip + " be added failed");
-                        }
-                        tryTimes++;
-                    } catch (InterruptedException e) {
-                        logger.info("The SR operate be sleep operation failed");
+            ExecResult execResult = Constants.ADD_BE.equals(command.getOpsType())
+                    ? StarRocksUtils.addBackendSqlClient(command.getFeMaster(), command.getHostName())
+                    : StarRocksUtils.addFollowerBySqlClient(command.getFeMaster(), command.getHostName());
+            String tip = Constants.ADD_BE.equals(command.getOpsType()) ? "backend" : "follower";
+            if (execResult.getExecResult()) {
+                logger.info(command.getHostName() + " " + tip + " be added success");
+            } else {
+                logger.info(command.getHostName() + " " + tip + " be added failed");
+            }
+            int tryTimes = 0;
+            while (!execResult.getExecResult() && tryTimes < 3) {
+                try {
+                    TimeUnit.SECONDS.sleep(10L);
+                    execResult = Constants.ADD_BE.equals(command.getOpsType())
+                            ? StarRocksUtils.addBackendSqlClient(command.getFeMaster(), command.getHostName())
+                            : StarRocksUtils.addFollowerBySqlClient(command.getFeMaster(), command.getHostName());
+                    if (execResult.getExecResult()) {
+                        logger.info(command.getHostName() + " " + tip + " be added success");
+                        break;
+                    } else {
+                        logger.info(command.getHostName() + " " + tip + " be added failed");
                     }
+                    tryTimes++;
+                } catch (InterruptedException e) {
+                    logger.info("The SR operate be sleep operation failed");
                 }
             }
         } else {
