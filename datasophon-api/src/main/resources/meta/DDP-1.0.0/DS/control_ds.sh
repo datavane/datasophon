dcd34507410777fd1e5a39e76bcfcbd3c7597d36
chenss <shaosongfirst@163.com>
2023-04-23 13:53:46 +0800

    according  HADOOP_HOME  to add hdfs config to Dolphinscheduer

--- datasophon-api/src/main/resources/meta/DDP-1.0.0/DS/control_ds.sh
+++ datasophon-api/src/main/resources/meta/DDP-1.0.0/DS/control_ds.sh
@@ -3,11 +3,25 @@ current_path=$(cd `dirname $0`;pwd)
 command=$1
 shift
 option=$1
+cd $current_path
+state=""
+function prepareEnv() {
+    process_name=$1
+    source /etc/profile
+    if [ -n "$HADOOP_HOME" ]
+        then
+        echo "sync hive conf "
+        cp $HADOOP_HOME/etc/hadoop/core-site.xml $current_path/$process_name/conf/
+        cp $HADOOP_HOME/etc/hadoop/hdfs-site.xml $current_path/$process_name/conf/
+    fi
+}
 
 function start() {
+
    process_name=$1
-   status $process_name
-   if [ $? -eq 0 ]
+   prepareEnv $process_name
+   execStatus $process_name
+   if [ $state -eq 0 ]
    then
     echo "ERROR: $process_name is running"
     exit 1
@@ -17,8 +31,8 @@ function start() {
 
 function stop() {
    process_name=$1
-   status $process_name
-   if [ $? -eq 1 ]
+   execStatus $process_name
+   if [ $state -eq 1 ]
    then
     echo "ERROR: $process_name is not running"
     exit 1
@@ -27,19 +41,31 @@ function stop() {
 }
 
 function status() {
-   process_name=$1
-   status=` $current_path/bin/dolphinscheduler-daemon.sh status $process_name | grep -v Begin | grep -v End | cut -d " " -f5`
-   echo "INFO: current status is $status"
-   if [[ $status="STOP"  ]]
-   then
-      return 1
-   fi
-   if [[ $status="RUNNING"  ]]
-   then
-      return 0
-   fi
-
+  process_name=$1
+  execStatus $process_name
+  if [ $state -eq 1 ]
+  then
+    echo "INFO: $process_name is not running"
+    exit 1
+  elif [ $state -eq 0 ]
+  then
+    echo "INFO: $process_name is running"
+    exit 0
+  fi
 }
+function execStatus() {
+  process_name=$1
+  status=` $current_path/bin/dolphinscheduler-daemon.sh status $process_name | grep -v Begin | grep -v End | cut -d " " -f5`
+  if [[ $status=="STOP"  ]]
+  then
+     state=1
+  fi
+  if [[ $status=="RUNNING"  ]]
+  then
+     state=0
+  fi
+}
+
 
 function restart() {
     stop $1
