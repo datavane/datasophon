e4b93b90d8be286dc260ac18bb0082eb8eb53d2e
zhzhenqin <zhzhenqin@163.com>
2023-04-26 14:22:47 +0800

    Manager Ping Worker, Worker State Check

--- datasophon-api/src/main/java/com/datasophon/api/master/HostCheckActor.java
+++ datasophon-api/src/main/java/com/datasophon/api/master/HostCheckActor.java
@@ -17,27 +17,41 @@
 
 package com.datasophon.api.master;
 
+import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
 import com.datasophon.api.service.ClusterHostService;
 import com.datasophon.api.service.ClusterInfoService;
 import com.datasophon.api.service.ClusterServiceRoleInstanceService;
 import com.datasophon.api.utils.SpringTool;
 import com.datasophon.common.command.HostCheckCommand;
+import com.datasophon.common.command.PingCommand;
+import com.datasophon.common.model.HostInfo;
+import com.datasophon.common.utils.ExecResult;
 import com.datasophon.common.utils.PromInfoUtils;
 import com.datasophon.common.utils.Result;
 import com.datasophon.dao.entity.ClusterHostEntity;
 import com.datasophon.dao.entity.ClusterInfoEntity;
 import com.datasophon.dao.entity.ClusterServiceRoleInstanceEntity;
-
+import com.datasophon.dao.enums.MANAGED;
 import org.apache.commons.lang.StringUtils;
-
-import java.util.List;
-import java.util.Objects;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
-import akka.actor.UntypedActor;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
+
+/**
+ * 节点状态监测
+ */
 public class HostCheckActor extends UntypedActor {
 
     private static final Logger logger = LoggerFactory.getLogger(HostCheckActor.class);
@@ -52,16 +66,51 @@ public class HostCheckActor extends UntypedActor {
                     SpringTool.getApplicationContext().getBean(ClusterServiceRoleInstanceService.class);
             ClusterInfoService clusterInfoService =
                     SpringTool.getApplicationContext().getBean(ClusterInfoService.class);
+
+            // Host or cluster
+            final HostCheckCommand hostCheckCommand = (HostCheckCommand)msg;
+            final HostInfo hostInfo = hostCheckCommand.getHostInfo();
+
+            // 获取当前安装并且正在运行的集群
             Result result = clusterInfoService.runningClusterList();
             List<ClusterInfoEntity> clusterList = (List<ClusterInfoEntity>) result.getData();
 
             for (ClusterInfoEntity clusterInfoEntity : clusterList) {
+                // 获取集群上安装的 Prometheus 服务, 从 Prometheus 获取CPU、磁盘使用量等
                 ClusterServiceRoleInstanceEntity prometheusInstance =
                         roleInstanceService.getOneServiceRole("Prometheus", "", clusterInfoEntity.getId());
                 if (Objects.nonNull(prometheusInstance)) {
+                    // 集群正常安装了 Prometheus
                     List<ClusterHostEntity> list = clusterHostService.getHostListByClusterId(clusterInfoEntity.getId());
                     String promUrl = "http://" + prometheusInstance.getHostname() + ":9090/api/v1/query";
                     for (ClusterHostEntity clusterHostEntity : list) {
+                        if(hostInfo != null && !StringUtils.equals(clusterHostEntity.getHostname(), hostInfo.getHostname())) {
+                            // 指定了节点，直接只处理这一个节点的
+                            continue;
+                        }
+                        try {
+                            // rpc 检测
+                            final ActorRef pingActor = ActorUtils.getRemoteActor(clusterHostEntity.getHostname(), "pingActor");
+                            PingCommand pingCommand = new PingCommand();
+                            pingCommand.setMessage("ping");
+                            Timeout timeout = new Timeout(Duration.create(180, TimeUnit.SECONDS));
+                            Future<Object> execFuture = Patterns.ask(pingActor, pingCommand, timeout);
+                            ExecResult execResult = (ExecResult) Await.result(execFuture, timeout.duration());
+                            if (execResult.getExecResult()) {
+                                logger.info("ping host: {} success", clusterHostEntity.getHostname());
+                            } else {
+                                logger.warn("ping host: {} fail, reason: {}", clusterHostEntity.getHostname(), execResult.getExecOut());
+                                throw new IllegalStateException("ping host: " + clusterHostEntity.getHostname() + " failed.");
+                            }
+                            clusterHostEntity.setHostState(1);
+                            clusterHostEntity.setManaged(MANAGED.YES);
+                        } catch (Exception e) {
+                            logger.warn("host: " + clusterHostEntity.getHostname() + " rpc error, cause: " + e.getMessage());
+                            clusterHostEntity.setHostState(3);
+                            clusterHostEntity.setManaged(MANAGED.NO);
+                            // ping 失败，则修改节点状态为 false
+                            continue;
+                        }
                         try {
                             String hostname = clusterHostEntity.getHostname();
                             // 查询内存总量
@@ -106,11 +155,50 @@ public class HostCheckActor extends UntypedActor {
                                 clusterHostEntity.setAverageLoad(cpuLoad);
                             }
                         } catch (Exception e) {
-                            logger.info(e.getMessage());
+                            logger.warn("check cluster state error, cause: {}", e.getMessage());
                         }
                     }
                     if (list.size() > 0) {
                         clusterHostService.updateBatchById(list);
+                    }
+                } else {
+                    // 没有 Prometheus？直接获取节点，通过 rpc 检测是否启动
+                    List<ClusterHostEntity> hosts = clusterHostService.getHostListByClusterId(clusterInfoEntity.getId());
+                    List<ClusterHostEntity> checkedHosts = new ArrayList<>(hosts.size());
+                    for (ClusterHostEntity host : hosts) {
+                        if(hostInfo != null && !StringUtils.equals(host.getHostname(), hostInfo.getHostname())) {
+                            // 指定了节点，直接只处理这一个节点的
+                            continue;
+                        }
+                        // copy 一个新的，只更新状态
+                        ClusterHostEntity checkedHost = new ClusterHostEntity();
+                        checkedHost.setId(host.getId());
+                        checkedHost.setCheckTime(new Date());
+                        try {
+                            // rpc 检测
+                            final ActorRef pingActor = ActorUtils.getRemoteActor(host.getHostname(), "pingActor");
+                            PingCommand pingCommand = new PingCommand();
+                            pingCommand.setMessage("ping");
+                            Timeout timeout = new Timeout(Duration.create(180, TimeUnit.SECONDS));
+                            Future<Object> execFuture = Patterns.ask(pingActor, pingCommand, timeout);
+                            ExecResult execResult = (ExecResult) Await.result(execFuture, timeout.duration());
+                            if (execResult.getExecResult()) {
+                                logger.info("ping host: {} success", host.getHostname());
+                            } else {
+                                logger.warn("ping host: {} fail, reason: {}", host.getHostname(), execResult.getExecOut());
+                                throw new IllegalStateException("ping host: " + host.getHostname() + " failed.");
+                            }
+                            checkedHost.setHostState(1);
+                            checkedHost.setManaged(MANAGED.YES);
+                        } catch (Exception e) {
+                            logger.warn("host: " + host.getHostname() + " rpc error, cause: " + e.getMessage());
+                            checkedHost.setManaged(MANAGED.NO);
+                            checkedHost.setHostState(3);
+                        }
+                        checkedHosts.add(checkedHost);
+                    }
+                    if (checkedHosts.size() > 0) {
+                        clusterHostService.updateBatchById(checkedHosts);
                     }
                 }
             }
