305ca93117c21477896c3dfadd0fcecffbd10ca6
zhzhenqin <zhzhenqin@163.com>
2023-07-05 10:26:56 +0800

    [Bug-316][manager] The Frame Service(Component) cannot be deleted when it installed

--- datasophon-api/src/main/java/com/datasophon/api/controller/FrameServiceController.java
+++ datasophon-api/src/main/java/com/datasophon/api/controller/FrameServiceController.java
@@ -17,25 +17,41 @@
 
 package com.datasophon.api.controller;
 
+import cn.hutool.core.io.FileUtil;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.datasophon.api.service.ClusterServiceInstanceService;
+import com.datasophon.api.service.FrameServiceRoleService;
 import com.datasophon.api.service.FrameServiceService;
+import com.datasophon.common.Constants;
 import com.datasophon.common.utils.Result;
+import com.datasophon.dao.entity.ClusterServiceInstanceEntity;
 import com.datasophon.dao.entity.FrameServiceEntity;
-
-import java.util.Arrays;
-import java.util.List;
-
+import com.datasophon.dao.entity.FrameServiceRoleEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("api/frame/service")
 public class FrameServiceController {
 
     @Autowired
     private FrameServiceService frameVersionServiceService;
+
+
+    @Autowired
+    private FrameServiceRoleService frameServiceRoleService;
+
+
+    @Autowired
+    private ClusterServiceInstanceService clusterServiceInstanceService;
 
     /**
      * 列表
@@ -84,12 +100,35 @@ public class FrameServiceController {
     }
 
     /**
-     * 删除
+     * 删除服务组件
      */
-    @RequestMapping("/delete")
-    public Result delete(@RequestBody Integer[] ids) {
-        frameVersionServiceService.removeByIds(Arrays.asList(ids));
+    @RequestMapping("/delete/{id}")
+    public Result delete(@PathVariable("id") Integer id) {
+        final FrameServiceEntity serviceEntity = frameVersionServiceService.getById(id);
+        if (serviceEntity == null) {
+            return Result.error("Service 组件不存在。");
+        }
+        // 已经安装为服务，无法删除
+        final List<ClusterServiceInstanceEntity> roleEntities = clusterServiceInstanceService.list(
+                Wrappers.<ClusterServiceInstanceEntity>lambdaQuery()
+                        .eq(ClusterServiceInstanceEntity::getFrameServiceId, serviceEntity.getId()));
+        if (roleEntities != null && !roleEntities.isEmpty()) {
+            return Result.error("Service 组件正在使用中。");
+        }
 
+        // delete /DDP/packages 下的软件包
+        File targetPackageFile = new File(Constants.MASTER_MANAGE_PACKAGE_PATH, serviceEntity.getPackageName());
+        FileUtil.del(targetPackageFile);
+        log.info("delete package file to: {}", targetPackageFile.getAbsolutePath());
+        File targetPackageFileMd5 = new File(Constants.MASTER_MANAGE_PACKAGE_PATH, serviceEntity.getPackageName() + ".md5");
+        FileUtil.del(targetPackageFileMd5);
+        log.info("delete package md5 file to: {}", targetPackageFileMd5.getAbsolutePath());
+
+        // 删除配置
+        frameServiceRoleService.remove(Wrappers.<FrameServiceRoleEntity>lambdaQuery()
+                .eq(FrameServiceRoleEntity::getServiceId, id));
+        // 删除主服务
+        frameVersionServiceService.removeById(id);
         return Result.success();
     }
 
