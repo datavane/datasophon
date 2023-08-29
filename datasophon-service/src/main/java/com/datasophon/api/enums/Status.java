5ae188785147b567e30277d2c32f3e86767f6ee1
zhaoxiaoyi <2498974539@qq.com>
2023-08-29 17:47:52 +0800

    commit content (#393)

--- datasophon-service/src/main/java/com/datasophon/api/enums/Status.java
+++ datasophon-service/src/main/java/com/datasophon/api/enums/Status.java
@@ -85,6 +85,7 @@ public enum Status {
     NEED_SAME_ROLE_GROUP(10039,
             "All instances of the same service on the same machine need to be within the same role group",
             "同一个服务在同一台机器上的所有实例需要在同一个角色组内"),
+    ODD_NUMBER_ARE_REQUIRED_FOR_DORISFE(10040, "The Number of DorisFE must be an odd number.", "DorisFE个数必须是奇数"),
             ;
 
     private final int code;
