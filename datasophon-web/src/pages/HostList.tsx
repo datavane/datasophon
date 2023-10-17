55f23be237444f07a945ce130afea24c178937de
Haitao Lee <lihaitaodesign@gmail.com>
2023-10-17 14:59:08 +0800

    chore: remove web from dev branch

--- datasophon-web/src/pages/HostList.tsx
+++
@@ -1,6 +0,0 @@
-import { PageContainer } from '@ant-design/pro-components'
-const HostList = () => {
-    return (<PageContainer header={{ title: '主机'}}></PageContainer>)
-}
-
-export default HostList
