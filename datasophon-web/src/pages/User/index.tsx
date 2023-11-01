89c2198b41b7f4fe37af998f795b989039dea020
Haitao Lee <lihaitaodesign@gmail.com>
2023-11-01 19:32:13 +0800

    feat: add operate button

--- datasophon-web/src/pages/User/index.tsx
+++ datasophon-web/src/pages/User/index.tsx
@@ -1,7 +1,10 @@
 import { PageContainer, ProColumns, ProTable } from '@ant-design/pro-components'
 import request from '../../services/request'
+import { Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 type UserType = {
+    id: number,
     username: string;
     password: string;
     email: string;
@@ -27,7 +30,27 @@ const columns: ProColumns<UserType>[] = [{
     title: '创建时间',
     search: false,
     dataIndex: 'createTime'
-  }]
+  },{
+    title: '操作',
+    valueType: 'option',
+    key: 'option',
+    render: (text, record, _, action) => [
+      <a
+        key="editable"
+        onClick={() => {
+        }}
+      >
+        编辑
+      </a>,
+      <a
+        key="delete"
+        onClick={() => {
+        }}
+      >
+        删除
+      </a>,
+    ],
+  },]
 
 const UserList = () => {
     return (
@@ -50,6 +73,17 @@ const UserList = () => {
                         success: code === 200
                     }
                   }}
+                toolBarRender={() => [
+                  <Button
+                    key="button"
+                    icon={<PlusOutlined />}
+                    onClick={() => {
+                    }}
+                    type="primary"
+                  >
+                    新建
+                  </Button>,
+                ]}
             ></ProTable>
         </PageContainer>
     )
