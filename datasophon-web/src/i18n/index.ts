9cbe7e6e9d3a2a5736a74fbb7aef353c33f6b27d
Haitao Lee <lihaitaodesign@gmail.com>
2023-11-04 13:34:56 +0800

    feat: backend request translation.json

--- datasophon-web/src/i18n/index.ts
+++ datasophon-web/src/i18n/index.ts
@@ -1,26 +1,12 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector  from 'i18next-browser-languagedetector' 
+import LanguageDetector  from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend'
 
 i18next
     .use(initReactI18next)
     .use(LanguageDetector)
+    .use(Backend)
     .init({
-        fallbackLng: 'en',
-        resources: {
-            zh: {
-                translation: {
-                    login: {
-                        tab: '账号密码登录'
-                    }
-                }
-            },
-            en: {
-                translation: {
-                    login: {
-                        tab: 'Login with account and password'
-                    }
-                }
-            }
-        }
+        fallbackLng: 'en'
     })
--- datasophon-web/src/main.tsx
+++ datasophon-web/src/main.tsx
@@ -7,6 +7,8 @@ import './i18n/index'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+      <React.Suspense fallback="loading">
+        <RouterProvider router={router} />
+      </React.Suspense>
   </React.StrictMode>,
 )
