2cb68e34930db09e9b481be401e554677db44f94
张克宇 <85506029+zhangkeyu008@users.noreply.github.com>
2023-12-14 10:53:35 +0800

    Branch 1.2.0 compile hbase2.5.6 (#476)
    * The deployment environment and resources are different, increasing the maximum communication time to ensure that HDFS can be successfully installed for the first time
    
    * 1. Compile hbase-2.5.6 and solve the problem as follows:
    
    (1) Solve integration issues with datasophon components, enabling one click deployment and management
    
    (2) Resolving the issue of hmaster having a probability of reboot failure during reboot
    
    (3) Resolve the incompatibility issue between HBase and Hadoop, which means that the snappy table cannot be created
    
    (4) Resolve compatibility issues with Phoenix, which means that Snappy tables and indexes cannot be created, making it compatible with Phoenix and able to create Snappy tables and indexes
    
    2. Modify the hbase env.ftl of the dataset worker to enable the newly compiled hbase-2.5.6 version to be monitored in the dataset
    
    ---------
    
    Co-authored-by: keyu <keyu@163.com>
    Co-authored-by: zhangkeyu <zhangkeyu@chinacomm.com>
    Co-authored-by: Gao Dayu <108419363+datasophon@users.noreply.github.com>

--- datasophon-worker/src/main/resources/script/datasophon-env.sh
+++ datasophon-worker/src/main/resources/script/datasophon-env.sh
@@ -6,10 +6,10 @@ export KYUUBI_HOME=/opt/datasophon/kyuubi-1.7.3
 export SPARK_HOME=/opt/datasophon/spark-3.1.3
 export PYSPARK_ALLOW_INSECURE_GATEWAY=1
 export HIVE_HOME=/opt/datasophon/hive-3.1.0
-
 export KAFKA_HOME=/opt/datasophon/kafka-2.4.1
-export HBASE_HOME=/opt/datasophon/hbase-2.4.16
-export FLINK_HOME=/opt/datasophon/flink-1.17.1
+export HBASE_HOME=/opt/datasophon/hbase-2.5.6
+export HBASE_PID_PATH_MK=/opt/datasophon/hbase-2.5.6/pid
+export FLINK_HOME=/opt/datasophon/flink-1.16.2
 export HADOOP_HOME=/opt/datasophon/hadoop-3.3.3
 export HADOOP_CONF_DIR=/opt/datasophon/hadoop-3.3.3/etc/hadoop
 export PATH=$PATH:$JAVA_HOME/bin:$SPARK_HOME/bin:$HADOOP_HOME/bin:$HIVE_HOME/bin:$FLINK_HOME/bin:$KAFKA_HOME/bin:$HBASE_HOME/bin
